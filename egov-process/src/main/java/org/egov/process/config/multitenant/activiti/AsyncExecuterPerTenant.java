57526e8c91d1dc9f1fb88629a1a626d8e3060cf4
sreekanth.nair <sreekanth.nair@egovernments.org>
2016-09-15 10:56:06 +0530

    Patching activiti's ExecuterPerTenantAsyncExecuter to solve NPE

---
+++ egov-process/src/main/java/org/egov/process/config/multitenant/activiti/AsyncExecuterPerTenant.java
@@ -0,0 +1,44 @@
+package org.egov.process.config.multitenant.activiti;
+
+import org.activiti.engine.impl.asyncexecutor.AsyncExecutor;
+import org.activiti.engine.impl.asyncexecutor.DefaultAsyncJobExecutor;
+import org.activiti.engine.impl.asyncexecutor.multitenant.ExecutorPerTenantAsyncExecutor;
+import org.activiti.engine.impl.asyncexecutor.multitenant.TenantAwareAcquireAsyncJobsDueRunnable;
+import org.activiti.engine.impl.asyncexecutor.multitenant.TenantAwareAcquireTimerJobsRunnable;
+import org.activiti.engine.impl.asyncexecutor.multitenant.TenantAwareExecuteAsyncRunnableFactory;
+import org.activiti.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.activiti.engine.impl.cfg.multitenant.TenantInfoHolder;
+
+public class AsyncExecuterPerTenant extends ExecutorPerTenantAsyncExecutor {
+    public AsyncExecuterPerTenant(TenantInfoHolder tenantInfoHolder) {
+        super(tenantInfoHolder, null);
+    }
+
+    public void addTenantAsyncExecutor(String tenantId, boolean startExecutor) {
+        AsyncExecutor tenantExecutor;
+
+        if (tenantAwareAyncExecutorFactory == null) {
+            tenantExecutor = new DefaultAsyncJobExecutor();
+        } else {
+            tenantExecutor = tenantAwareAyncExecutorFactory.createAsyncExecutor(tenantId);
+        }
+
+        tenantExecutor.setProcessEngineConfiguration(processEngineConfiguration); // Needs to be done for job executors created after boot. Doesn't hurt on boot.
+
+        if (tenantExecutor instanceof DefaultAsyncJobExecutor) {
+            DefaultAsyncJobExecutor defaultAsyncJobExecutor = (DefaultAsyncJobExecutor) tenantExecutor;
+            defaultAsyncJobExecutor.setAsyncJobsDueRunnable(new TenantAwareAcquireAsyncJobsDueRunnable(defaultAsyncJobExecutor, tenantInfoHolder, tenantId));
+            defaultAsyncJobExecutor.setTimerJobRunnable(new TenantAwareAcquireTimerJobsRunnable(defaultAsyncJobExecutor, tenantInfoHolder, tenantId));
+            defaultAsyncJobExecutor.setExecuteAsyncRunnableFactory(new TenantAwareExecuteAsyncRunnableFactory(tenantInfoHolder, tenantId));
+        }
+        tenantExecutors.put(tenantId, tenantExecutor);
+
+        if (startExecutor) {
+            tenantExecutor.start();
+        }
+    }
+
+    public void setProcessEngineConfiguration(ProcessEngineConfigurationImpl processEngineConfiguration) {
+          this.processEngineConfiguration = processEngineConfiguration;
+    }
+}
