f8c615413327503b0ea136410507d2a43a9f60c6
Manikanta <manikanta@egovernments.org>
2016-09-21 18:19:58 +0530

    adding sample users and groups

---
+++ egov-process/src/main/resources/db/migration/main/sample_users_group.sql
@@ -0,0 +1,11 @@
+INSERT INTO eg_usergroup VALUES (1, 'Superusers', 'security-role');
+INSERT INTO eg_usergroup VALUES (2, 'financials', 'assignment');
+INSERT INTO eg_usergroup VALUES (3, 'Assets', 'assignment');
+
+
+
+INSERT INTO eg_user(id,firstname,lastname,email,username,password) VALUES (1,  'admin', 'Administrator', 'admin', 'test', NULL);
+INSERT INTO eg_user(id,firstname,lastname,email,username,password) VALUES (2, 'mani', 'kanta', 'manikanta@egovernments.org','mani', 'egov');
+INSERT INTO eg_user(id,firstname,lastname,email,username,password) VALUES (3, 'kavya', 'kavya', 'kavya.ys@egovernments.org', 'kavya', 'egov');
+INSERT INTO eg_user(id,firstname,lastname,email,username,password) VALUES (4, 'venki', 'venki', 'venki@egovernments.org','venki', 'egov' );
+INSERT INTO eg_user(id,firstname,lastname,email,username,password) VALUES (5, 'elzan', 'mathew', 'elzan.mathew@egovernments.org','elzan', 'egov' );
