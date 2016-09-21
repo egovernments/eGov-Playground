da9337fabfa79a1837e5a3ebd8f739183ebd81e3
Manikanta <manikanta@egovernments.org>
2016-09-21 18:25:27 +0530

    documented the exection flow

--- egov-process/src/main/java/org/egov/process/web/controller/NumberGenerationController.java
+++ egov-process/src/main/java/org/egov/process/web/controller/NumberGenerationController.java
@@ -6,7 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+/**
+ * 
+ * @author mani
+ *
+ *  Sample execution flow
+ *  http://localhost:8180/egov-process/gen/start?message=startingon21sep
+ *	http://localhost:8180/egov-process/gen/process?userName=kavya&message=kavyaworking&processInstaceId=91a89322-7fde-11e6-9292-34e6ad894cd4
+ *	http://localhost:8180/egov-process/gen/process?userName=venki&message=venkiworking&processInstaceId=6acce775-7fcf-11e6-a94e-34e6ad894cd4
 
+ */
 @RestController
 public class NumberGenerationController{
 	private static  Logger Log=Logger.getLogger(NumberGenerationController.class);
