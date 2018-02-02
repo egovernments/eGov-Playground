3323df1eac4664966491a718c37bebb106c52c44
Manikanta <manikanta@egovernments.org>
2018-02-02 12:50:20 +0530

    Measure distance to road

---
+++ eGov-edcr/src/main/java/org/egov/edcr/J21.java
@@ -0,0 +1,131 @@
+
+package org.egov.edcr;
+
+
+import java.awt.Polygon;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.kabeja.dxf.DXFConstants;
+import org.kabeja.dxf.DXFDocument;
+import org.kabeja.dxf.DXFEntity;
+import org.kabeja.dxf.DXFLWPolyline;
+import org.kabeja.dxf.DXFLayer;
+import org.kabeja.dxf.DXFLine;
+import org.kabeja.dxf.DXFMText;
+import org.kabeja.dxf.DXFVertex;
+import org.kabeja.dxf.helpers.Point;
+import org.kabeja.parser.DXFParser;
+import org.kabeja.parser.Parser;
+import org.kabeja.parser.ParserBuilder;
+import org.springframework.util.ResourceUtils;
+
+public class J21 {
+
+    public static void main(String[] args) {
+
+        Parser parser = ParserBuilder.createDefaultParser();
+
+        try {
+            //File Path
+            File file = ResourceUtils.getFile("classpath:dxf/kj21.dxf");
+            String path = file.getPath();
+           
+
+            //Parse DXF File
+            parser.parse(path, DXFParser.DEFAULT_ENCODING);
+
+            //Extract DXF Data
+            DXFDocument doc = parser.getDocument();
+
+            getHeightOfTheBuilding(doc);
+            
+            Util util= new Util();
+          DXFLine line= util.getSingleLineByLayer(doc, "Shortest Distance to road");
+          
+          if(line !=null && line.getLength()< 3)
+          {
+        	  System.err.println("Shortest Distance to road condition violated");
+          }else
+          {
+        	  System.out.println(" Shortest Distance to road condition is accepted");
+          }
+            
+          List<DXFLWPolyline> frontYardLines= util.getPolyLinesByLayer(doc, "Front yard");
+          if(frontYardLines.size()==1)
+          {
+        	  DXFLWPolyline frontYard=  frontYardLines.get(0);
+        	/*  System.out.println(frontYard.getColumns());
+        	  System.out.println(frontYard.getBounds().getMaximumY());
+        	  System.out.println(frontYard.getBounds().getMinimumY());
+        	*/ 
+        	  System.out.println("front yard distance"+Math.abs(frontYard.getBounds().getMaximumY()-frontYard.getBounds().getMinimumY()));
+          }
+          
+          List<DXFLWPolyline> rearYards= util.getPolyLinesByLayer(doc, "Rear yard");
+          if(rearYards.size()==1)
+          {
+        	  DXFLWPolyline frontYard=  rearYards.get(0);
+        	/*  System.out.println(frontYard.getColumns());
+        	  System.out.println(frontYard.getBounds().getMaximumY());
+        	  System.out.println(frontYard.getBounds().getMinimumY());
+        	*/ 
+        	  System.out.println("rear yard distance"+Math.abs(frontYard.getBounds().getMaximumY()-frontYard.getBounds().getMinimumY()));
+          }
+            // getBlocks(doc);
+            
+            Map<String, String> planInfoProperties = util.getPlanInfoProperties(doc);
+            System.out.println("Plot Area="+planInfoProperties.get("PLOT_AREA"));
+
+            Iterator dxfLayerIterator = doc.getDXFLayerIterator();
+
+                
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+	private static void getBlocks(DXFDocument doc) {
+		int block=0;
+        while(doc.getDXFBlockIterator().hasNext() )
+        {
+		  block++;
+		  doc.getDXFBlockIterator().next();
+        }
+        
+        System.out.println("blocks"+block);
+	}
+    
+    private static double  getHeightOfTheBuilding(DXFDocument doc)
+    {
+    	double height=0;
+    	height= doc.getBounds().getHeight();
+    	System.out.println("height"+height);
+    	return height;
+    	
+    }
+
+    private static double polygonArea(ArrayList<Double> x, ArrayList<Double> y, int numPoints) {
+
+        double area = 0;         // Accumulates area in the loop
+        int j = numPoints - 1;  // The last vertex is the 'previous' one to the first
+
+        for (int i = 0; i < numPoints; i++) {
+            area = area + (x.get(j) + x.get(i)) * (y.get(j) - y.get(i));
+            j = i;  //j is previous vertex to i
+        }
+
+        return area / 2;
+    }
+
+
+}    
+
--- eGov-edcr/src/main/java/org/egov/edcr/Util.java
+++ eGov-edcr/src/main/java/org/egov/edcr/Util.java
@@ -3,144 +3,264 @@ package org.egov.edcr;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.kabeja.dxf.DXFConstants;
 import org.kabeja.dxf.DXFDocument;
 import org.kabeja.dxf.DXFLWPolyline;
 import org.kabeja.dxf.DXFLayer;
+import org.kabeja.dxf.DXFLine;
 import org.kabeja.dxf.DXFPolyline;
+import org.kabeja.dxf.DXFText;
 import org.kabeja.dxf.DXFVertex;
 import org.kabeja.dxf.helpers.Point;
 
 public class Util {
 
-    public List<DXFLWPolyline> getPolyLinesByColor(DXFDocument dxfDocument,  Integer colorCode) {
+	public List<DXFLWPolyline> getPolyLinesByColor(DXFDocument dxfDocument, Integer colorCode) {
 
-        List<DXFLWPolyline> dxflwPolylines = new ArrayList<>();
+		List<DXFLWPolyline> dxflwPolylines = new ArrayList<>();
 
-        Iterator dxfLayerIterator = dxfDocument.getDXFLayerIterator();
+		Iterator dxfLayerIterator = dxfDocument.getDXFLayerIterator();
 
-        while (dxfLayerIterator.hasNext()) {
+		while (dxfLayerIterator.hasNext()) {
 
-            DXFLayer dxfLayer = (DXFLayer) dxfLayerIterator.next();
+			DXFLayer dxfLayer = (DXFLayer) dxfLayerIterator.next();
 
-            List dxfPolyLineEntities = dxfLayer.getDXFEntities(DXFConstants.ENTITY_TYPE_LWPOLYLINE);
+			List dxfPolyLineEntities = dxfLayer.getDXFEntities(DXFConstants.ENTITY_TYPE_LWPOLYLINE);
 
-            if (null != dxfPolyLineEntities) {
-                for (Object dxfEntity : dxfPolyLineEntities) {
+			if (null != dxfPolyLineEntities) {
+				for (Object dxfEntity : dxfPolyLineEntities) {
 
-                    DXFLWPolyline dxflwPolyline = (DXFLWPolyline) dxfEntity;
+					DXFLWPolyline dxflwPolyline = (DXFLWPolyline) dxfEntity;
 
-                   
-                        if (colorCode == dxflwPolyline.getColor()) {
-                            dxflwPolylines.add(dxflwPolyline);
-                        }
-                    }
-                }
-            }
-        
+					if (colorCode == dxflwPolyline.getColor()) {
+						dxflwPolylines.add(dxflwPolyline);
+					}
+				}
+			}
+		}
 
-        return dxflwPolylines;
-    }
-    
-    public List<DXFLWPolyline> getPolyLinesByColors(DXFDocument dxfDocument, List<Integer> colorCodes) {
+		return dxflwPolylines;
+	}
 
-        List<DXFLWPolyline> dxflwPolylines = new ArrayList<>();
+	public List<DXFLine> getLinesByColor(DXFDocument dxfDocument, Integer color) {
 
-        Iterator dxfLayerIterator = dxfDocument.getDXFLayerIterator();
+		List<DXFLine> lines = new ArrayList<>();
 
-        while (dxfLayerIterator.hasNext()) {
+		Iterator dxfLayerIterator = dxfDocument.getDXFLayerIterator();
 
-            DXFLayer dxfLayer = (DXFLayer) dxfLayerIterator.next();
+		while (dxfLayerIterator.hasNext()) {
 
-            List dxfPolyLineEntities = dxfLayer.getDXFEntities(DXFConstants.ENTITY_TYPE_LWPOLYLINE);
+			DXFLayer dxfLayer = (DXFLayer) dxfLayerIterator.next();
 
-            if (null != dxfPolyLineEntities) {
-                for (Object dxfEntity : dxfPolyLineEntities) {
+			List dxfPolyLineEntities = dxfLayer.getDXFEntities(DXFConstants.ENTITY_TYPE_LINE);
 
-                    DXFLWPolyline dxflwPolyline = (DXFLWPolyline) dxfEntity;
+			if (null != dxfPolyLineEntities) {
+				for (Object dxfEntity : dxfPolyLineEntities) {
 
-                    for (int colorCode : colorCodes) {
-                        if (colorCode == dxflwPolyline.getColor()) {
-                            dxflwPolylines.add(dxflwPolyline);
-                        }
-                    }
-                }
-            }
-        }
+					DXFLine line = (DXFLine) dxfEntity;
 
-        return dxflwPolylines;
-    }
-    
-    public List<DXFLWPolyline> getPolyLinesByLayer(DXFDocument dxfDocument, String name) {
+					if (color == line.getColor()) {
+						lines.add(line);
+					}
 
-        List<DXFLWPolyline> dxflwPolylines = new ArrayList<>();
+				}
+			}
+		}
 
-        Iterator dxfLayerIterator = dxfDocument.getDXFLayerIterator();
+		return lines;
+	}
 
-        while (dxfLayerIterator.hasNext()) {
+	public List<DXFLine> getLinesByLayer(DXFDocument dxfDocument, String name) {
 
-            DXFLayer dxfLayer = (DXFLayer) dxfLayerIterator.next();
+		List<DXFLine> lines = new ArrayList<>();
 
-            List dxfPolyLineEntities = dxfLayer.getDXFEntities(DXFConstants.ENTITY_TYPE_LWPOLYLINE);
+		Iterator dxfLayerIterator = dxfDocument.getDXFLayerIterator();
 
-            if (null != dxfPolyLineEntities) {
-                for (Object dxfEntity : dxfPolyLineEntities) {
+		while (dxfLayerIterator.hasNext()) {
 
-                    DXFLWPolyline dxflwPolyline = (DXFLWPolyline) dxfEntity;
+			DXFLayer dxfLayer = (DXFLayer) dxfLayerIterator.next();
 
-                    
-                        if (name.equalsIgnoreCase(dxflwPolyline.getLayerName())) {
-                            dxflwPolylines.add(dxflwPolyline);
-                       
-                    }
-                }
-            }
-        }
+			List dxfPolyLineEntities = dxfLayer.getDXFEntities(DXFConstants.ENTITY_TYPE_LINE);
 
-        return dxflwPolylines;
-    }
+			if (null != dxfPolyLineEntities) {
+				for (Object dxfEntity : dxfPolyLineEntities) {
 
+					DXFLine line = (DXFLine) dxfEntity;
 
-    private BigDecimal getPolyLineArea(DXFPolyline dxfPolyline) {
+					if (name == line.getLayerName()) {
+						lines.add(line);
+					}
 
-        ArrayList x = new ArrayList();
-        ArrayList y = new ArrayList();
+				}
+			}
+		}
 
-        Iterator vertexIterator = dxfPolyline.getVertexIterator();
+		return lines;
+	}
 
-        //Vertex and coordinates of Polyline
-        while (vertexIterator.hasNext()) {
+	public DXFLine getSingleLineByLayer(DXFDocument dxfDocument, String name) {
 
-            DXFVertex dxfVertex = (DXFVertex) vertexIterator.next();
-            Point point = dxfVertex.getPoint();
+		if(dxfDocument==null)
+			return null;
+		if(name==null)
+			return null;
+		
+		List<DXFLine> lines = new ArrayList<>();
 
-            //values needed to calculate area
-            x.add(point.getX());
-            y.add(point.getY());
+		Iterator dxfLayerIterator = dxfDocument.getDXFLayerIterator();
 
-        }
+		while (dxfLayerIterator.hasNext()) {
 
-        return polygonArea(x, y, dxfPolyline.getVertexCount());
-    }
+			DXFLayer dxfLayer = (DXFLayer) dxfLayerIterator.next();
 
-    //Using ShoeLace Formula to calculate area of polygon
-    private BigDecimal polygonArea(ArrayList<Double> x, ArrayList<Double> y, int numPoints) {
+			List dxfLineEntities = dxfLayer.getDXFEntities(DXFConstants.ENTITY_TYPE_LINE);
 
-        double area = 0;         // Accumulates area in the loop
-        int j = numPoints - 1;  // The last vertex is the 'previous' one to the first
+			if (null != dxfLineEntities) {
+				for (Object dxfEntity : dxfLineEntities) {
 
-        for (int i = 0; i < numPoints; i++) {
-            area = area + (x.get(j) + x.get(i)) * (y.get(j) - y.get(i));
-            j = i;  //j is previous vertex to i
-        }
+					DXFLine line = (DXFLine) dxfEntity;
 
-        BigDecimal convertedArea = new BigDecimal(area / 2);
+					if (name.equalsIgnoreCase(line.getLayerName())) {
+						lines.add(line);
+					}
 
-        return convertedArea.setScale(4, RoundingMode.HALF_UP).abs();
+				}
+			}
+		}
+		if (lines.size() == 1) {
+			return lines.get(0);
+		} else
+			return null;
 
-    }
+	}
+
+	public List<DXFLWPolyline> getPolyLinesByColors(DXFDocument dxfDocument, List<Integer> colorCodes) {
+
+		List<DXFLWPolyline> dxflwPolylines = new ArrayList<>();
+
+		Iterator dxfLayerIterator = dxfDocument.getDXFLayerIterator();
+
+		while (dxfLayerIterator.hasNext()) {
+
+			DXFLayer dxfLayer = (DXFLayer) dxfLayerIterator.next();
+
+			List dxfPolyLineEntities = dxfLayer.getDXFEntities(DXFConstants.ENTITY_TYPE_LWPOLYLINE);
+
+			if (null != dxfPolyLineEntities) {
+				for (Object dxfEntity : dxfPolyLineEntities) {
+
+					DXFLWPolyline dxflwPolyline = (DXFLWPolyline) dxfEntity;
+
+					for (int colorCode : colorCodes) {
+						if (colorCode == dxflwPolyline.getColor()) {
+							dxflwPolylines.add(dxflwPolyline);
+						}
+					}
+				}
+			}
+		}
+
+		return dxflwPolylines;
+	}
+
+	public List<DXFLWPolyline> getPolyLinesByLayer(DXFDocument dxfDocument, String name) {
+
+		List<DXFLWPolyline> dxflwPolylines = new ArrayList<>();
+
+		Iterator dxfLayerIterator = dxfDocument.getDXFLayerIterator();
+
+		while (dxfLayerIterator.hasNext()) {
+
+			DXFLayer dxfLayer = (DXFLayer) dxfLayerIterator.next();
+
+			List dxfPolyLineEntities = dxfLayer.getDXFEntities(DXFConstants.ENTITY_TYPE_LWPOLYLINE);
+
+			if (null != dxfPolyLineEntities) {
+				for (Object dxfEntity : dxfPolyLineEntities) {
+
+					DXFLWPolyline dxflwPolyline = (DXFLWPolyline) dxfEntity;
+
+					if (name.equalsIgnoreCase(dxflwPolyline.getLayerName())) {
+						dxflwPolylines.add(dxflwPolyline);
+
+					}
+				}
+			}
+		}
+
+		return dxflwPolylines;
+	}
+
+	private BigDecimal getPolyLineArea(DXFPolyline dxfPolyline) {
+
+		ArrayList x = new ArrayList();
+		ArrayList y = new ArrayList();
+
+		Iterator vertexIterator = dxfPolyline.getVertexIterator();
+
+		// Vertex and coordinates of Polyline
+		while (vertexIterator.hasNext()) {
+
+			DXFVertex dxfVertex = (DXFVertex) vertexIterator.next();
+			Point point = dxfVertex.getPoint();
+
+			// values needed to calculate area
+			x.add(point.getX());
+			y.add(point.getY());
+
+		}
+
+		return polygonArea(x, y, dxfPolyline.getVertexCount());
+	}
+
+	// Using ShoeLace Formula to calculate area of polygon
+	private BigDecimal polygonArea(ArrayList<Double> x, ArrayList<Double> y, int numPoints) {
+
+		double area = 0; // Accumulates area in the loop
+		int j = numPoints - 1; // The last vertex is the 'previous' one to the
+								// first
+
+		for (int i = 0; i < numPoints; i++) {
+			area = area + (x.get(j) + x.get(i)) * (y.get(j) - y.get(i));
+			j = i; // j is previous vertex to i
+		}
+
+		BigDecimal convertedArea = new BigDecimal(area / 2);
+
+		return convertedArea.setScale(4, RoundingMode.HALF_UP).abs();
+
+	}
+	
+ public static	Map<String,String>	getPlanInfoProperties(DXFDocument doc)
+	{
+		
+		DXFLayer planInfoLayer = doc.getDXFLayer("Plan info");
+		List texts = planInfoLayer.getDXFEntities(DXFConstants.ENTITY_TYPE_MTEXT);
+		String param="";
+		DXFText	text=null;
+		Map<String,String> planInfoProperties=new HashMap<>();
+		while(texts.iterator().hasNext())
+		{
+			text= (DXFText)texts.iterator().next();
+			param=text.getText();
+			System.out.println(param);
+			String[] data = param.split("=");
+			if(data.length==2)
+			{
+				planInfoProperties.put(data[0], data[1]);
+			}else
+			{
+				//throw new RuntimeException("Plan info sheet data not following standard '=' for " +param);
+			}
+		}
+		return planInfoProperties;
+	
+	}
+ 
 
 }
